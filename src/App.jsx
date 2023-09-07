313c8cf27e36b86240c6f940c1e4768d9f898e9c
Claudio Florio <claudioflorio1@hotmail.com>
2023-09-07 09:46:06 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import Contact from './components/Contact'
+import Hero from './components/Hero'
+import {Who} from './components/Who'
+import {Works} from './components/Works'
+import styled from "styled-components";
+
+import './App.css';
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
+function App() {
+
+  return (
+      <Container>
+        <Hero/>
+        <Who/>
+        <Works/>
+        <Contact/>
+      </Container>
+  )
+}
+
+export default App
