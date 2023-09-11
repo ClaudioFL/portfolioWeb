0fa0a01de7e041f73708a25f46e1e052a89fc097
Claudio Florio <claudioflorio1@hotmail.com>
2023-09-11 15:59:06 -0400

    Worked on works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import './App.css';
-import Contact from "./components/Contact"
-import Hero from './components/Hero'
-import Who from './components/Who'
-import Works from './components/Works'
 import styled from "styled-components";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import "./global.css"
+
+
 
 const Container = styled.div`
-  height: 100vh;
-  background-color: rebeccapurple;
-  color: white;
-  background: url("/img/bg.jpeg");
+  height: 110vh;
   scroll-snap-type: y mandatory;
   scroll-behavior: smooth;
   overflow-y: auto;
   scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
   &::-webkit-scrollbar{
     display: none;
+    padding: 0;
   }
-`
-function App() {
+  margin: 0;
+  padding: 0;
+`;
 
-  return (
-      <Container>
-        <Hero/>
-        <Who/>
-        <Works/>
-        <Contact/>
-      </Container>
-  )
+function App() {
+    return (
+        <Container>
+            <Hero />
+            <Who />
+            <Works />
+            <Contact />
+        </Container>
+    );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const Section = styled.div`
   display: flex;
   justify-content: center;
-`
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
 const Container = styled.div`
   width: 1400px;
   display: flex;
   justify-content: space-between;
-  padding: 10px;
-`
+  align-items: center;
+  padding: 10px 0px;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    padding: 10px;
+  }
+`;
 
 const Links = styled.div`
   display: flex;
   align-items: center;
-  gap: 5px;
+  gap: 50px;
 `;
 
 const Logo = styled.img`
-  height:50px;
+  height: 50px;
 `;
 
 const List = styled.ul`
   display: flex;
   gap: 20px;
   list-style: none;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const ListItem = styled.li`
@@ -59,7 +73,7 @@ const Navbar = () => {
         <Section>
             <Container>
                 <Links>
-                    <Logo src="./img/logo.png"/>
+                    <Logo src="./img/logo.png" />
                     <List>
                         <ListItem>Home</ListItem>
                         <ListItem>Studio</ListItem>
@@ -68,12 +82,13 @@ const Navbar = () => {
                     </List>
                 </Links>
                 <Icons>
-                    <Icon src={"./img/search.png"}/>
-                    <Button>Hire Me</Button>
+                    {/* Changed the image due to copyright problems */}
+                    <Icon src="./img/search.png" />
+                    <Button>Hire Now</Button>
                 </Icons>
             </Container>
         </Section>
-    )
-}
+    );
+};
 
 export default Navbar;
