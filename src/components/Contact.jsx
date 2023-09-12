e1ccdbb76759c9b7a309820a8221b0494250a4c1
Claudio Florio <claudioflorio1@hotmail.com>
2023-09-12 09:41:35 -0400

    Added a map still need to change the specific location of said map.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Map from "./Map";
 
 const Section = styled.div`
   height: 100vh;
@@ -86,7 +87,9 @@ const Contact = () => {
                      <Button>Send</Button>
                  </Form>
              </Left>
-             <Right></Right>
+             <Right>
+                 <Map/>
+             </Right>
          </Container>
         </Section>
     )
